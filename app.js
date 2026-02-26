100b8fa04775cdb3fa306763b605e6a828fd475c
Molnii26 <molnii26.26@gmail.com>
2026-02-26 14:08:56 +0100

    cors options

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 const userRoutes = require('./routes/userRoutes')
 
@@ -8,6 +9,10 @@ const cookieParser = require('cookie-parser')
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 
 app.use('/users/', userRoutes)
 
